62e05980d279b0330fbffc2a10e316fef7890e74
GPlay97 <GPlay97@users.noreply.github.com>
2020-04-11 01:34:33 +0200

    prepare history and submitData

--- controllers/sync.js
+++ controllers/sync.js
@@ -1,13 +1,22 @@
 const asyncHandler = require('@evnotify/utils').asyncHandler;
-const SettingsModel = require('../models/Settings');
+const HistoryModel = require('../models/History');
+const SyncModel = require('../models/Sync');
 const errors = require('../errors.json');
 
-const deleteSettings = asyncHandler(async(req, res, next) => {
-    await SettingsModel.deleteOne({
-        akey: req.params.akey
-    });
+const submitData = asyncHandler(async(req, res, next) => {
+    // TODO outsource this in @evnotify/utils?!
+    if (req.params.akey !== req.headers.AKey) return next(errors.AKEY_MISMATCH);
 
-    res.status(204).end();
+    const syncObj = req.body;
+
+    syncObj.akey = req.params.akey;
+    // create a history record
+    await HistoryModel.create(syncObj);
+    // sync to log?! trigger
+    // update last sync
+    res.json(await SyncModel.updateOne({
+        akey: req.params.akey
+    }, req.body));
 });
 
 module.exports = {
---
+++ models/History.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const connection = require('@evnotify/utils').db.getDB();
+
+const TelemetrySchema = require('./Telemetry');
+const LocationSchema = require('./Location');
+
+const options = {
+    id: false,
+    collection: 'sync',
+    timestamps: true,
+    toObject: {
+        getters: true
+    },
+    versionKey: false
+};
+
+const HistorySchema = new mongoose.Schema({
+    akey: {
+        type: String,
+        required: true
+    },
+    telemetry: TelemetrySchema,
+    location: LocationSchema
+}, options);
+
+module.exports = connection.model('History', HistorySchema);
