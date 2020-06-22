e4235b7718ab3f220b941213d9ea1a8262d181b7
GPlay97 <GPlay97@users.noreply.github.com>
2020-06-22 19:13:47 +0200

    add push to log service; fix akey check

--- controllers/sync.js
+++ controllers/sync.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const asyncHandler = require('@evnotify/utils').asyncHandler;
 const HistoryModel = require('../models/History');
 const SyncModel = require('../models/Sync');
@@ -5,18 +6,34 @@ const errors = require('../errors.json');
 
 const submitData = asyncHandler(async(req, res, next) => {
     // TODO outsource this in @evnotify/utils?!
-    if (req.params.akey !== req.headers.AKey) return next(errors.AKEY_MISMATCH);
+    if (req.params.akey !== req.headers.akey) return next(errors.AKEY_MISMATCH);
 
     const syncObj = req.body;
 
     syncObj.akey = req.params.akey;
     // create a history record
     await HistoryModel.create(syncObj);
-    // sync to log?! trigger
-    // update last sync
-    res.json(await SyncModel.updateOne({
-        akey: syncObj.akey
-    }, syncObj));
+
+    // push to log service
+    axios.post(process.env.LOG_SERVICE + '/latest', syncObj, {
+        headers: {
+            'Authorization': req.headers.authorization,
+            'Authentication': req.headers.authentication,
+            'AKey': syncObj.akey
+        }
+    }).then(async() => {
+        // update last sync
+        res.json(await SyncModel.updateOne({
+            akey: syncObj.akey
+        }, syncObj));
+    }).catch((err) => {
+        try {
+            next(err.response.data.error || 500);
+        } catch (error) {
+            console.error(error);
+            next(500);
+        }
+    });
 });
 
 module.exports = {
